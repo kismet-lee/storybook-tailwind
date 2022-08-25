057e02ee9de99653ad25890feaf3f845282dfc73
Destiny Simmons <34193988+kismet-lee@users.noreply.github.com>
2022-08-25 21:17:04 +0900

    Primary and Secondary button classes
    Added tailwind classes to primary and secondary buttons on storybook.js.
    
    Will continue to add the rest of the buttons and tags incrementally.

--- src/classnames.tsx
+++ src/classnames.tsx
@@ -1,3 +1,8 @@
 export const button = {
-    primary: 'bg-blue-500 enabled:hover:bg-blue-900 text-white py-2 px-4 rounded-md disabled:bg-blue-400'
+    primary: 'bg-blue-500 enabled:hover:bg-blue-900 text-white text-lg py-3 px-6 rounded-lg disabled:bg-blue-300',
+    ntrl:'bg-gray-900 text-white text-lg  py-3 px-6 rounded-lg',
+    small: 'bg-blue-500 enabled:hover:bg-blue-900 text-white text-md py-2 px-6 rounded-lg disabled:bg-blue-300',
+    smntrl: 'bg-gray-900 text-white text-md  py-2 px-6 rounded-lg',
+    secondary: 'border-2 border-blue-500 enabled:hover:border-blue-900 enabled:hover:text-blue-900 text-blue-500 text-lg py-3 px-6 rounded-lg disabled:border-blue-300 disabled:text-blue-300',
+    secondaryntrl:'border-2 border-gray-900 text-gray-900 text-lg  py-3 px-6 rounded-lg'
 }
--- src/stories/TailwindButton.stories.tsx
+++ src/stories/TailwindButton.stories.tsx
@@ -20,3 +20,60 @@ export const PrimaryDisabled = () => {
     </div>
   )
 }
+
+export const PrimaryNeutral = () => {
+  return (
+    <div>
+      <button className={button.ntrl}>some action</button>
+    </div>
+  )
+}
+
+export const PrimarySmall = () => {
+  return (
+    <div>
+      <button className={button.small}>some action</button>
+    </div>
+  )
+}
+
+export const PrimarySmallDisabled = () => {
+  return (
+    <div>
+      <button className={button.small} disabled>some action</button>
+    </div>
+  )
+}
+
+
+export const PrimarySmallNeutral = () => {
+  return (
+    <div>
+      <button className={button.smntrl}>some action</button>
+    </div>
+  )
+}
+
+export const Secondary = () => {
+  return (
+    <div>
+      <button className={button.secondary}>some action</button>
+    </div>
+  )
+}
+
+export const SecondaryDisabled = () => {
+  return (
+    <div>
+      <button className={button.secondary} disabled>some action</button>
+    </div>
+  )
+}
+
+export const SecondaryNeutral = () => {
+  return (
+    <div>
+      <button className={button.secondaryntrl}>some action</button>
+    </div>
+  )
+}
